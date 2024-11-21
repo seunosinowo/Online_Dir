c700b5f758176acdeb82094c70e317140bce0d46
seunosinowo <oluwaseunpaul98@gmail.com>
2024-11-21 10:30:29 +0100

    Copyright box

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Copyright } from 'lucide-react'
 const Footer = () => {
   return (
     <div className='flex space-x-3'>
-      <div className='bg-black w-full flex justify-center'>
+      <div className='bg-black w-full flex justify-center items-center h-9'>
         <div className='text-white flex flex-row space-x-2'>
             <Copyright/>
             <p>Osinowo Oluwaseun</p>
